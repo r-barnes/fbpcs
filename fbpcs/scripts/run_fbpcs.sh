a5d978a0642a28957ae6b40b45fa5fc7e0e51021
Brian Muse <brianmuse@fb.com>
2022-11-02 08:56:46 -0700

    Remove debug statement

--- fbpcs/scripts/run_fbpcs.sh
+++ fbpcs/scripts/run_fbpcs.sh
@@ -168,7 +168,6 @@ function run_fbpcs() {
   if [[ "$FBPCS_CONTAINER_REPO_URL" == *"amazonaws"* ]]; then
     aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin "$FBPCS_CONTAINER_REPO_URL"
   fi
-  echo "Environment variables ${environment_vars[@]}"
   docker pull "$docker_image"
   set -o xtrace
   docker run "${environment_vars[@]}" --rm \
